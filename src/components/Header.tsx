25e7cf38b43aa085dc02097e6461a1e27a859b2b
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2023-10-12 19:10:19 +0900

    Header 내 모든 요소를 세로 축을 기준으로 가운데 정렬한다

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Container = styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding-inline: .6em;
 
     img {
