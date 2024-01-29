2aee61f72946999d9d44ff868cd5df45b850f0ba
kaori-killer <75800958+kaori-killer@users.noreply.github.com>
2024-01-29 15:45:33 +0900

    지도 페이지의 하단 배너 구현

---
+++ src/components/Map/BannerContent.tsx
@@ -0,0 +1,93 @@
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+    max-width: 600px;
+    width: 100%;
+    box-shadow: 0px 2px 15px 5px rgba(1, 60, 169, 0.15);
+    position: fixed;
+    bottom: 75px;
+    z-index: 1;
+    height: 105px;
+    background-color: #FFFFFF;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-left: 15px;
+    padding-right: 15px;
+    cursor: grab;
+`;
+
+const SlideImg = styled.div`
+    border: 0px;
+    background-color: #BBC7D3;
+    border-radius: 12px;
+    width: 44px;
+    height: 5.747px;
+    margin: 11px;
+
+`;
+
+const FilterContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-top: 5px;
+
+    button {
+      border: 0px;
+      border-radius: 30px;
+      font-size: 13px;
+      font-style: normal;
+      font-weight: 510;
+      line-height: normal;
+      letter-spacing: -0.2px;
+      margin-left: 0.5rem;
+      margin-right: 0.5rem;
+      box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.12);
+      cursor: pointer;
+    }
+`;
+
+const DayFilterContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 40%;
+
+  button {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+const CategoryFilterContanier = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 60%;
+
+  button {
+    width: 90px;
+    height: 50px;
+  }
+`;
+
+export default function BannerContent() {
+  return (
+    <Container>
+      <SlideImg />
+      <FilterContainer>
+        <DayFilterContainer>
+          <button type="button">월</button>
+          <button type="button">화</button>
+          <button type="button">수</button>
+        </DayFilterContainer>
+        <CategoryFilterContanier>
+          <button type="button">🍺 주점</button>
+          <button type="button">🎡 비주점</button>
+          <button type="button">🍕 푸드트럭</button>
+        </CategoryFilterContanier>
+      </FilterContainer>
+    </Container>
+  );
+}
---
+++ src/utils/CreateKakaoMap.ts
@@ -0,0 +1,58 @@
+export default function Kakao() {
+  const { kakao } = window;
+
+  const plan = (x: number, y: number, z: number) => {
+    y = -y - 1;
+    const limit = Math.ceil(3 / 2 ** z);
+    const path = 'split_map/';
+
+    if (y >= 0 && y < limit && x >= 0 && x < limit) {
+      return `${path}planh${
+        z}_${y}_${x}.png`;
+    }
+    return 'https://i1.daumcdn.net/dmaps/apis/white.png';
+  };
+
+  kakao.maps.Tileset.add(
+    'PLAN',
+    new kakao.maps.Tileset(512, 512, plan, '', false, 0, 2),
+  );
+
+  const node = document.getElementById('map');
+  const map = new kakao.maps.Map(node, {
+    projectionId: null,
+    mapTypeId: kakao.maps.MapTypeId.PLAN,
+    $scale: false,
+    // 지도의 중심 좌표 (x, y)
+    center: new kakao.maps.Coords(750, -700),
+    level: 0,
+  });
+  const center = map.getCenter();
+  const marker = new kakao.maps.Marker({
+    position: center,
+  });
+  marker.setMap(map);
+
+  // // 커스텀 오버레이에 표시할 내용입니다
+  // // HTML 문자열 또는 Dom Element 입니다
+  // const content = '<div class ="label"><span class="left"></span><button class="center">안녕</button><span class="right"></span></div>';
+
+  // // 커스텀 오버레이가 표시될 위치입니다
+  // const position = new kakao.maps.Coords(700, -700);
+
+  // // 커스텀 오버레이를 생성합니다
+  // const customOverlay = new kakao.maps.CustomOverlay({
+  //   position,
+  //   content,
+  // });
+
+  // // console.log(customOverlay);
+
+  // // 커스텀 오버레이를 지도에 표시합니다
+  // customOverlay.setMap(map);
+
+  // const infowindow = new kakao.maps.InfoWindow({
+  //   content: '커스텀 타일셋?',
+  // });
+  // infowindow.open(map, marker);
+}
