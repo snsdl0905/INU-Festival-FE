ad36add99d4995f60046f61b51b5756b4f46f1c4
heeju <hxxzxx506@gmail.com>
2024-01-29 07:43:36 -0500

    로그인 컴포넌트 삭제하고 합침

--- src/components/Login/LoginContainer.tsx
+++ src/components/Login/LoginContainer.tsx
@@ -9,7 +9,23 @@ flex-direction: column;
 align-items: center;
 gap: 10px;
 `;
-
+const IsLoginBtn = styled.button`
+display: flex;
+height: 48px;
+justify-content: center;
+align-items: center;
+align-self: stretch;
+border-radius: 12px;
+border:none;
+background: #0147C8;
+color: #FFF;
+font-family: "SF Pro";
+font-size: 15px;
+font-style: normal;
+font-weight: 510;
+line-height: normal;
+letter-spacing: -0.6px;
+`;
 const KakaoBtn = styled.button`
 display: flex;
 height: 48px;
@@ -105,7 +121,7 @@ export default function LoginContainer() {
         type="password"
       />
       <BtnWrapper>
-        <LoginBtn />
+        <IsLoginBtn type="submit">로그인</IsLoginBtn>
         <KakaoBtn onClick={handleLogin}>카카오톡으로 로그인</KakaoBtn>
       </BtnWrapper>
       <Explain>
