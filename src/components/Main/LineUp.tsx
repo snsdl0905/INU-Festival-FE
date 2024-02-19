8c521ad9dbbcc4b5a688ebc1de63c3897a2c4a95
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-02-19 21:28:37 +0900

    라인업의 시작위치를 변경했다

--- src/components/Main/LineUp.tsx
+++ src/components/Main/LineUp.tsx
@@ -35,6 +35,7 @@ export default function LineUp({
       speed={300}
       pagination={{ clickable: true }}
       allowTouchMove
+      slidesOffsetBefore={17}
     >
       {lineups.map((lineup, index) => {
         const key = `${index}-${lineup.id}`;
