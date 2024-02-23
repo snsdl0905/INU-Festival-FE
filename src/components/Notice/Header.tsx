c3d1c895ddcc006a4f68e5e789d09253ea7b7fec
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-02-24 05:12:37 +0900

    Header의 unknown prop 에러 해결

--- src/components/Notice/Header.tsx
+++ src/components/Notice/Header.tsx
@@ -7,12 +7,12 @@ type HeaderProps = {
     shadow: 'true' | 'false';
 }
 
-const HeaderContainer = styled.div <{ shadow: string }>`
+const HeaderContainer = styled.div <{ $shadow: string }>`
 display: flex;
 align-items: center;
 padding-inline: ${(props) => props.theme.sizes.contentPadding};
 padding-block: 1.4em;
-box-shadow: ${(props) => (props.shadow === 'true' ? '0px 0.3rem 0.3rem 0px rgba(0, 0, 0, 0.05)' : 'none')};
+box-shadow: ${(props) => (props.$shadow === 'true' ? '0px 0.3rem 0.3rem 0px rgba(0, 0, 0, 0.05)' : 'none')};
 
 span{
   margin: 0 auto;
@@ -31,7 +31,7 @@ button{
 export default function Header({ children, shadow }: HeaderProps) {
   const navigate = useNavigate();
   return (
-    <HeaderContainer shadow={shadow}>
+    <HeaderContainer $shadow={shadow}>
       <button type="button" onClick={() => navigate(-1)} aria-label="Go Back">
         <svg xmlns="http://www.w3.org/2000/svg" width="11" height="20" viewBox="0 0 11 20" fill="none">
           <path fillRule="evenodd" clipRule="evenodd" d="M0.706858 9.19451C0.316334 9.58503 0.316334 10.2182 0.706858 10.6087L9.19426 19.0961C9.58479 19.4866 10.2179 19.4866 10.6085 19.0961C10.999 18.7056 10.999 18.0724 10.6085 17.6819L2.82818 9.90162L10.6085 2.12132C10.999 1.7308 10.999 1.09763 10.6085 0.707107C10.2179 0.316582 9.58479 0.316582 9.19426 0.707107L0.706858 9.19451Z" fill="black" />
