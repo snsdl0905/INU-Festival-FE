c2daa4aa9250ac9a3fa049b3a9ca0034b3532d79
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-03-26 18:30:14 +0900

    Revert "Revert "방명록의 채팅 기능을 구현했다." build 오류 발생"
    This reverts commit 38e9e2e0ffef35b4f3210444dcda117d140fd833, reversing
    changes made to 0199988ffa6f83d6f093cab625f75f133d2cd01f.

--- src/components/Notice/NoticeBottom.tsx
+++ src/components/Notice/NoticeBottom.tsx
@@ -5,38 +5,47 @@ import { styled } from 'styled-components';
 
 import Notice from '../../types/Notice';
 import useCheckScreenWidth from '../../hooks/useCheckScreenWidth';
+import ImageModal from './ImageModal';
 
 const NoticeContentBox = styled.div`
-background-color: #EBF2FF;
-margin-top: 2.3rem;
-padding: 2rem;
-border-radius: 10px;
-font-size: 15px;
-font-style: normal;
-font-weight: 400;
-line-height: 24px;
-letter-spacing: -0.6px;
-white-space: pre-line;
+  background-color: #EBF2FF;
+  margin-top: 2.3rem;
+  padding: 2rem;
+  border-radius: 10px;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: -0.6px;
+  white-space: pre-line;
 `;
 
 const NoticeImageContainer = styled.div`
-border-radius: 1rem;
-display: flex;
+  border-radius: 1rem;
+  display: flex;
 `;
 
 const NoticeImageBox = styled.img`
-width: 15rem;
-border-radius: 1rem;
-margin: 2rem 0.6rem;
+  width: 15rem;
+  border-radius: 1rem;
+  margin: 2rem 0.6rem;
+  cursor: pointer;
 `;
 
 type NoticeItemProps = {
-    notice: Notice;
+  notice: Notice;
 }
 
 export default function NoticeBottom({ notice } : NoticeItemProps) {
   const [perview, setPerView] = useState(3);
+  const [modal, setModal] = useState(false);
+  const [image, setImage] = useState('');
   const defaultPerview = 3;
+
+  const handleImageClick = (src: string) => {
+    setModal(() => true);
+    setImage(() => src);
+  };
 
   useCheckScreenWidth(defaultPerview, setPerView);
 
@@ -56,13 +65,18 @@ export default function NoticeBottom({ notice } : NoticeItemProps) {
               const key = `${img.id} + ${img.img}`;
               return (
                 <SwiperSlide key={key}>
-                  <NoticeImageBox src={img.img} alt={img.img} />
+                  <NoticeImageBox
+                    src={img.img}
+                    alt={img.img}
+                    onClick={() => handleImageClick(img.img)}
+                  />
                 </SwiperSlide>
               );
             })
           }
         </NoticeImageContainer>
       </Swiper>
+      {modal && <ImageModal img={image} setModal={setModal} />}
     </NoticeContentBox>
   );
 }
