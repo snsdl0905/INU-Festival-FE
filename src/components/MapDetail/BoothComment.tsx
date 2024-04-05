e99112dc7aed98941868dbe16265c9dbf0b805bb
heeju <hxxzxx506@gmail.com>
2024-04-05 15:41:22 +0900

    이모지 데이터 추가

--- src/components/MapDetail/BoothComment.tsx
+++ src/components/MapDetail/BoothComment.tsx
@@ -56,9 +56,11 @@ export default function BoothComment({ boothId }: string) {
     <>
       {boothComments.map((
         boothCommentDetail:
-        { userId: string; content:string; createdAt:string; },
+        { userId: string; content:string; createdAt:string; emoji : string;},
       ) => {
-        const { userId, content, createdAt } = boothCommentDetail;
+        const {
+          userId, content, createdAt, emoji,
+        } = boothCommentDetail;
         const createdAtDate = new Date(createdAt);
 
         const formattedDateTime = createdAtDate.toLocaleString(undefined, {
@@ -68,18 +70,7 @@ export default function BoothComment({ boothId }: string) {
         return (
           <CommentBox key={userId}>
             <CommentTop>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24Z" fill="url(#paint0_linear_546_1836)" />
-                <path d="M6.73062 11.8398L9.21205 10.0464L6.73062 8.25293" stroke="#2D2D2D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M17.2687 11.8398L14.7864 10.0464L17.2687 8.25293" stroke="#2D2D2D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <path d="M6.13657 16.0244L9.06823 17.8965L11.999 16.0244L14.9306 17.8965L17.8623 16.0244" stroke="#2D2D2D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                <defs>
-                  <linearGradient id="paint0_linear_546_1836" x1="3.42402" y1="3.74696" x2="20.7754" y2="20.4449" gradientUnits="userSpaceOnUse">
-                    <stop stopColor="#FEC99F" />
-                    <stop offset="1" stopColor="#F25963" />
-                  </linearGradient>
-                </defs>
-              </svg>
+              <img src={`${emoji}.svg`} alt="이모지" />
               <h3>{userId}</h3>
               <div>{formattedDateTime}</div>
             </CommentTop>
