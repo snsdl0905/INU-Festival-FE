f53a4d82b7124ee1dc38945ed341b81c52652082
heeju <hxxzxx506@gmail.com>
2024-05-06 19:30:30 +0900

    메인으로 가면 자동으로 상단으로 스크롤된다.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Article from '../components/Article';
 import Footer from '../components/Main/Footer';
 import Lineups from '../components/Main/Lineups';
@@ -5,6 +6,9 @@ import FestivalSentence from '../components/Main/FestivalSentence';
 import BoothRanking from '../components/Main/BoothRanking';
 
 export default function MainPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <Lineups />
